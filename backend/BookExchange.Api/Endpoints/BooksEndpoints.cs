778cf1ea8ac17ba63372518dd6bc4d64b17a0bd8
Zach Privett <z.privett92@gmail.com>
2025-07-14 21:17:59 -0400

    added async to endpoints

--- backend/BookExchange.Api/Endpoints/BooksEndpoints.cs
+++ backend/BookExchange.Api/Endpoints/BooksEndpoints.cs
@@ -10,55 +10,23 @@ public static class BooksEndpoints
 {
     const string GetBookEndpointName = "GetBook";
 
-    private static readonly List<BookSummaryDto> books = [
-        new (
-            1, 
-            "Without Remorse", 
-            "Tom Clancy", 
-            "Mility Thriller", 
-            "970-0-425-14332-2", 
-            "Ok", 
-            "Military/Spy thriller. Action",
-            780
-        ),
-        new (
-            2, 
-            "The Way of Kings", 
-            "Brandon Sanderson", 
-            "Epic Fantasy", 
-            "670-0-323-17892-1", 
-            "Good", 
-            "Fantasy World. Magic. Epic quests",
-            1120
-        ),
-        new (
-            3, 
-            "The Name of the Wind", 
-            "Patrick Rothfuss", 
-            "Epic Fantasy", 
-            "795-0-465-19212-5", 
-            "Good", 
-            "Follows Kvothe, an inkeeper who narates his epic conquest through the world",
-            850
-        )
-    ];
-
     public static RouteGroupBuilder MapBooksEndpoints(this WebApplication app) 
     {
         var group = app.MapGroup("books").WithParameterValidation();  
 
         // GET /books
-        group.MapGet("/", (BookExchangeContext dbContext) =>
-            dbContext.Books
+        group.MapGet("/", async (BookExchangeContext dbContext) =>
+            await dbContext.Books
                     .Include(book => book.Genre)
                     .Include(book => book.Condition)
                     .Select(book => book.ToBookSummaryDto())
-                    .AsNoTracking());
+                    .AsNoTracking()
+                    .ToListAsync());
 
         // GET /books/1
-        group.MapGet("/{id}", (int id, BookExchangeContext dbContext) =>
+        group.MapGet("/{id}", async (int id, BookExchangeContext dbContext) =>
         {
-            Book? book = dbContext.Books.Find(id);
+            Book? book = await dbContext.Books.FindAsync(id);
 
             return book is null ? Results.NotFound() :
                 Results.Ok(book.ToBookDetailsDto());
@@ -66,12 +34,12 @@ public static class BooksEndpoints
         .WithName(GetBookEndpointName);
 
         // POST /books
-        group.MapPost("/", (CreateBookDto newBook, BookExchangeContext dbContext) =>
+        group.MapPost("/", async (CreateBookDto newBook, BookExchangeContext dbContext) =>
         {
             Book book = newBook.ToEntity();
 
             dbContext.Books.Add(book);
-            dbContext.SaveChanges();
+            await dbContext.SaveChangesAsync();
 
             return Results.CreatedAtRoute(
                 GetBookEndpointName,
@@ -80,9 +48,9 @@ public static class BooksEndpoints
         });
 
         // PUT /books
-        group.MapPut("/{id}", (int id, UpdateBookDto updatedBook, BookExchangeContext dbContext) => 
+        group.MapPut("/{id}", async (int id, UpdateBookDto updatedBook, BookExchangeContext dbContext) => 
         {
-            var existingBook = dbContext.Books.Find(id);
+            var existingBook = await dbContext.Books.FindAsync(id);
 
             if (existingBook is null)
             {
@@ -91,15 +59,15 @@ public static class BooksEndpoints
 
             dbContext.Entry(existingBook).CurrentValues.SetValues(updatedBook.ToEntity(id));
 
-            dbContext.SaveChanges();
+            await dbContext.SaveChangesAsync();
 
             return Results.NoContent();
         });
 
         // DELETE /book/1
-        group.MapDelete("/{id}", (int id, BookExchangeContext dbContext) => 
+        group.MapDelete("/{id}", async (int id, BookExchangeContext dbContext) => 
         {
-            dbContext.Books.Where(book => book.Id == id).ExecuteDelete();
+            await dbContext.Books.Where(book => book.Id == id).ExecuteDeleteAsync();
 
             return Results.NoContent();
         });
